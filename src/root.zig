9b1a5797f640bb4f0a8864b4e63c5eb55183f964
sarpagarud <241446179+sarpagarud@users.noreply.github.com>
2026-08-23 23:52:27 +0530

    Update root.zig

--- src/root.zig
+++ src/root.zig
@@ -2,6 +2,8 @@
 const std = @import("std");
 const Io = std.Io;
 
+const core_zig = @import("core.zig");
+
 /// This is a documentation comment to explain the `printAnotherMessage` function below.
 ///
 /// Accepting an `Io.Writer` instance is a handy way to write reusable code.
